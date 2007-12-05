d6f2dad400d2437aa6ec019a5e8233b04ce24bfa
stephen <stephen@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-12-05 10:54:41 +0000

    Added outline and some skeleton files
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/graphicsqc@3 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/compare.R
@@ -0,0 +1,16 @@
+# --------------------------------------------------------------------
+# compare.R
+# --------------------------------------------------------------------
+
+# --------------------------------------------------------------------
+#
+# compare()
+#
+# compare will take two(?) image files of the same format and identify
+# any differences in the files.
+# compare has the option of deleting the test file and the model file
+# once the comparison has been made, and then to retain files if they
+# are different.
+#
+# --------------------------------------------------------------------
+"compare" <- function(){}
