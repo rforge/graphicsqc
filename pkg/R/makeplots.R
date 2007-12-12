c6d0100709eb8fdd9411f5ba307a255ff14f477b
stephen <stephen@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-12-12 21:49:43 +0000

    updated makeplots.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/graphicsqc@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/makeplots.R
+++ pkg/R/makeplots.R
@@ -14,16 +14,90 @@
 # MUST always work (=> tryCatch)
 #
 # Return value is record of files created
-#
+# source("~/graphicsqc/makeplots.R")
 # --------------------------------------------------------------------
 "plotExpr" <- function(expr, # character vector
                              # R expression(s)
-                       format = NULL, # character vector
+                       filetype = NULL, # character vector
                                       # (valid) file formats
                        prefix = NULL, # char length 1
                                       # file prefix
-                       path = NULL, # char length 1
+                       path = NULL # char length 1
                                     # directory
                        ) {
-    tryCatch(eval(parse(text="plot(1:10)")))
+                       
+    # First split expression up by ";" in case multiple expressions are stacked
+    # with each other
+    if(is.list(expr))
+        expr <- unlist(expr)
+    expr <- unlist(strsplit(expr,split=";"))
+        
+    ##Test filetype is valid, prefix, path
+    
+    # Testing filetype is valid
+    filetype<-tolower(filetype)
+    if (testValidFiletypes())
+        #change "ps" to "postscript"
+    
+    # Testing file prefix is valid
+    #prefix must be length 1 OR length expr
+    if (length(prefix) == 1) || length(prefix) == length(expr))
+        prefix<-as.character(prefix) else
+        stop("prefix must be length 1 or same as expr")
+    
+    ##Change this so it runs multiple paths?
+    if (length(path) > 1) {
+        warning("object ", sQuote("path"), " has more than one ",
+                "element: only the first used in ", sQuote("path"), 
+                call. = FALSE)
+        path <- path[1]
+    }
+    
+    
+    
+    
+    numFiles <- length(expr)
+    nameFormat <- paste(prefix,"-%0",nchar(numFiles),"d",sep="") 
+#   filenames <- sprintf(nameFormat,prefix,1:numFiles)
+    # result <- list of length(filetype)
+    for(i in 1:length(filetype))
+        #open filetype
+        # or openGraphics(filetype[i])
+        result <- sapply(expr, evalPlotCode) #result[i]
+    #graphics.off()
+    invisible(result)
 }
+
+"evalPlotCode" <- function(expr) {
+    print("evalPlotCode called with:")
+#      tryCatch(eval(parse(text = paste("Print(\"Tried to print:",expr,"\")"))), 
+ #                        error = function(e) { 
+  #                                   paste("Error in", expr, ":",
+   #                                  geterrmessage())
+    #                             }, 
+     #                    warning = function(w) {
+      #                                 paste("Warning in", expr, 
+       #                                ":", conditionMessage(w))
+        #                           })
+}
+
+#------------------------------
+#
+# testValidFiletypes()
+#
+#------------------------------
+"testValidFiletypes" <- function(filetypes) {
+    validFiletypes <- c("pdf", "png", "ps", "bmp")
+    if (.Platform$OS.type != "windows")
+        validFiletypes<-validFiletypes[-4]
+
+    if(any(duplicated(filetypes))) {
+        warning("duplicated filetypes!.. ...at? ..removed/ignored?")
+    }  else if (all(filetypes %in% validFiletypes)) {
+        return(TRUE)
+    } else {
+        warning("this filetype is not valid...")
+        return(FALSE)
+    }
+}
+
