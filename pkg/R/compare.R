c577f92c3982e6a93bdceefda05ec26b2e59e2a7
paul <paul@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-12-18 02:34:18 +0000

    compare shell
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/graphicsqc@12 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/compare.R
+++ pkg/R/compare.R
@@ -13,9 +13,31 @@
 # are different.
 #
 # --------------------------------------------------------------------
-"compare" <- function(path,
+"compare" <- function(test,
+                      control,
                       clean = c("full", "files", "none"), # char length 1
                                     # none = don't delete anything
                                     # full = delete everything
                                     # files = delete files (but NOT index)
                                     # AFTER EACH expr
+                      ) {
+
+    if (is.character(test)) {
+        if ("it's one or more log files") {
+        } else {
+            # It's a PATH (not a file) !
+            # so autodetect log files
+        }
+        
+    } else if (is.qcresult(test)) {
+        
+    } else {
+        stop()
+    }
+
+    # DITTO for control
+    
+    # Compare one plotExpr at a time (using natural order of qcresult
+    # OR order specified explicitly by log files OR the natural
+    # order of the log files from the autodetect).
+}
